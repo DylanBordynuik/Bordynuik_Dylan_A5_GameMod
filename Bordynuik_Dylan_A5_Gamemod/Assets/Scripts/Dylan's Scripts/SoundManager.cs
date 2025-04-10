0133d8184f81036600134d34ba6ff6d33a02e573
DylanBordynuik <DYLANBORDYNUIK2@GMAIL.COM>
2025-04-10 13:15:16 -0400

    Add SoundManager
    Fleshed out the "SoundManager" script, added background music through the script.

--- Bordynuik_Dylan_A5_Gamemod/Assets/Scripts/Dylan's Scripts/SoundManager.cs
+++ Bordynuik_Dylan_A5_Gamemod/Assets/Scripts/Dylan's Scripts/SoundManager.cs
@@ -1,19 +1,38 @@
 using UnityEngine;
 
-public class ShootSound : MonoBehaviour
+public class BackgroundMusic : MonoBehaviour
 {
 
-    public AudioClip backgroundMusic;  // The sound to play
-    private AudioSource audioSource;
+    public AudioClip soundClip;  // the sound to play
+    private AudioSource musicSource;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        musicSource = gameObject.AddComponent<AudioSource>();
+
+        if (soundClip == null)
+        {
+            soundClip = Resources.Load<AudioClip>("ChipTune"); // Assuming the audio file is placed in the Resources folder
+        }
+
+        if (soundClip != null)
+        {
+            musicSource.clip = soundClip;
+            musicSource.loop = true;  //loop the sound, its background music after all!
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        // check if the sound isn't already playing
+        if (!musicSource.isPlaying)
+        {
+            musicSource.Play();
+        }
+
     }
 }
